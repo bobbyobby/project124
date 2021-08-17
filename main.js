fbb34305d2995cc0b843fbf84bbc8ab424c1eaec
bobbyobby <78395958+bobbyobby@users.noreply.github.com>
2021-08-16 19:01:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=m15.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+function modelLoaded(){
+    console.log('Posenet is Ready');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function draw(){
+    background('#808080');
+}
